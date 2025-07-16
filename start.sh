99c522ab841c23538e4fd2a1ad1924a878e54f69
jcfernandezgithub <jcfernandez@outlook.com.ar>
2025-07-16 19:41:43 -0300

    fixes

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
- #!/bin/bash
-
-# Instalar playwright (solo si no está instalado)
-playwright install --with-deps
-
-# Iniciar servidor
+playwright install
 uvicorn app.main:app --host 0.0.0.0 --port 10000
